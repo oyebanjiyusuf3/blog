f06fbe37c0bde1d54d88bc50f509a133e19592e4
oyebanjiyusuf3 <oyebanjiyusuf3@gmail.com>
2023-06-03 20:55:12 +0100

    user panel: post category

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,6 +82,9 @@
     </div>
     <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
         <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+            @foreach($categories as $category)
+                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">{{$category->title}}</a>
+            @endforeach
             <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
             <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
             <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
